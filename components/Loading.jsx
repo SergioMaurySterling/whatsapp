1d2e1f509367c52b4347f2f8447ece27fba318f5
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-12-12 17:28:39 -0500

    Loading component

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,7 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+import styled from 'styled-components'
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
 export default function Loading() {
   return (
-    <div>
-      <h1>LOADING</h1>
-    </div>
+    <center style={{display: "grid", placeItems:"center", height: "100vh"}}>
+      <Container>
+        <img
+          src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png'
+          alt='wapp logo'
+          style={{ marginBottom: 10}}
+          height={200}
+        />
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      </Container>
+    </center>
   )
 }
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whitesmoke;
+`;
